1e4da11d2b7617ec334f6b4f748c673dbd02aa96
Raphael <raphael.ferreira@hetic.net>
2020-06-30 17:14:12 +0200

    complete burger menu

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -1,17 +1,59 @@
 <template>
-  <section class="menu"></section>
+  <section class="menu">
+    <Title text="whatorder" class="menu__title" />
+    <ul class="menu__links">
+      <li class="menu__link">MCU</li>
+      <li class="menu__link">Fast&furious</li>
+      <li class="menu__link">XMens</li>
+      <li class="menu__link">Infos</li>
+    </ul>
+  </section>
 </template>
 <script>
+import Title from "@/components/Title.vue";
+
 export default {
   name: "Burger",
+  components: {
+    Title,
+  },
 };
 </script>
 <style lang="scss" scoped>
 .menu {
   position: absolute;
   z-index: 1;
-  height: 100%;
+  height: 100vh;
   width: 100%;
-  background: black;
+  background: var(--mainDark);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+
+  &__title,
+  &__link {
+    color: var(--mainWhite);
+  }
+
+  &__title {
+    font-size: var(--smallTitle);
+  }
+
+  &__links {
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 50%;
+  }
+
+  &__link {
+    text-decoration: underline;
+    font-family: var(--font);
+    font-size: 36px;
+    text-transform: uppercase;
+  }
 }
 </style>
