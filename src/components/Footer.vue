1557f05641cb2eb1ffae176423f57145ab2e14b1
Raphael <raphael.ferreira@hetic.net>
2020-07-04 10:36:39 +0200

    patch footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <p>
       whatorder is a personnal project created to understand Vue.js. You can
       find the code on
-      <a target="_blank" href="https://github.com/rqphy/whatorder">GitHub</a>.
+      <a target="_blank " href="https://github.com/rqphy/whatorder">GitHub</a>.
     </p>
   </footer>
 </template>
@@ -19,9 +19,15 @@ footer {
   height: 250px;
   width: 100%;
   font-family: var(--font);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
+  font-size: 14px;
+  line-height: 21px;
 }
 p {
+  width: 80%;
   margin-top: 20px;
   color: var(--mainDark);
 }
