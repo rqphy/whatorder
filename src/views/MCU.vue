64967838f2a835e537f6351de0f0c43054da12ef
Raphael <raphael.ferreira@hetic.net>
2020-07-04 11:12:32 +0200

    adapt mcu page to new container component

--- src/views/MCU.vue
+++ src/views/MCU.vue
@@ -1,73 +1,16 @@
 <template>
-  <div class="container">
-    <Burger v-if="openMenu" />
-    <Title v-if="!openMenu" text="whatorder" />
-    <Illustration v-if="!openMenu" theme="mcu" />
-    <Infos
-      v-if="!openMenu"
-      :watchTime="infos.time"
-      :length="infos.movies.length"
-    />
-    <Movies v-if="!openMenu" :movies="infos.movies" />
-    <Gradient v-if="!openMenu" />
-    <div class="burger" @click="updateOpenMenu">
-      <div class="burger__background" v-if="!openMenu">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="28"
-          height="28"
-          class="burger__icon"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"
-            fill="rgba(245,245,245,1)"
-          />
-        </svg>
-      </div>
-      <div class="burger__background burger__background--open" v-if="openMenu">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="28"
-          height="28"
-          class="burger__icon"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"
-            fill="rgba(5,5,5,1)"
-          />
-        </svg>
-      </div>
-    </div>
-    <Footer />
-  </div>
+  <Container :infos="infos" />
 </template>
 <script>
-import Title from "@/components/Title.vue";
-import Illustration from "@/components/Illustration.vue";
-import Infos from "@/components/Infos.vue";
-import Movies from "@/components/Movies.vue";
-import Burger from "@/components/Burger.vue";
-import Gradient from "@/components/Gradient.vue";
-import Footer from "@/components/Footer.vue";
+import Container from "@/components/Container.vue";
 
 export default {
   name: "MCU",
   components: {
-    Title,
-    Illustration,
-    Infos,
-    Movies,
-    Burger,
-    Gradient,
-    Footer,
+    Container,
   },
   data() {
     return {
-      openMenu: false,
       infos: {
         time: "32 Hours",
         movies: [
@@ -105,32 +48,4 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.burger {
-  height: 56px;
-  width: 56px;
-  position: fixed;
-  z-index: 2;
-  bottom: 70px;
-  right: 5%;
-
-  &__background {
-    border-radius: 50%;
-    background: var(--mainDark);
-    height: 100%;
-    width: 100%;
-    &--open {
-      background: var(--mainWhite);
-    }
-  }
-  &__icon {
-    transform: translate(50%, 50%);
-  }
-}
-</style>
+<style lang="scss" scoped></style>
